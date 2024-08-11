229dccc98fdcce529804e489de5ed9a9ff1644d0
Admin <ava4@student.waikati.ac.nz>
2024-08-12 11:09:51 +1200

    Test

---
+++ Front end/src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Home from "./screens/home/home.js";
+
+export default function App() {
+  return (
+  <div>
+    <Home />
+  </div>
+  );
+}
---
+++ Front end/src/components/LoginForm/LoginForm.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './LoginForm.css'
+
+const LoginForm = () => {
+  return (
+    <div>
+      <LoginForm />
+    </div>
+  );
+}
+
+export default LoginForm
